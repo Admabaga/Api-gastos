3615b0bcfff538d2691019873cd5ed98933a6a0a
LeonDavid-ctrl <david.mejia.leon421@gmail.com>
2024-11-08 08:09:35 -0500

    Refactor cors

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ Base.metadata.create_all(bind = engine)
 app=FastAPI()
 
 #Config cors
-origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
